f708e706f989fbf97280345875d666bcca94801d
Daniel Zucchi <danielzucchi@Daniels-MacBook-Pro.local>
2018-06-13 18:30:53 +0100

    connect to remote Mongo database

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var     commentRoutes    = require("./routes/comments"),
         indexRoutes      = require("./routes/index"),
         userRoutes       = require("./routes/user")
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://danielzucchi:Cbl533629@ds259410.mlab.com:59410/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
